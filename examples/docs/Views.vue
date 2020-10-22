e2f09d02ea605fc0dd07393da019a9e3193e0bf2
lee <xiaopeng_li@qingsongchou.com>
2020-10-22 17:26:24 +0800

    完成tree递归组件

--- examples/docs/Views.vue
+++ examples/docs/Views.vue
@@ -5,13 +5,41 @@
         <!-- <lee-divider position="left" :leftDashed="true" :rightDashed="false" leftHeight="15px" leftLineColor="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1602849726859&di=e4ba67718d65acc56a197b1af073271f&imgtype=0&src=http%3A%2F%2Fa0.att.hudong.com%2F56%2F12%2F01300000164151121576126282411.jpg"><span style="color: red;">我是分割线</span></lee-divider> -->
         <!-- <lee-input>姓名</lee-input> -->
         <!-- <lee-date-picker></lee-date-picker> -->
-        <lee-tree></lee-tree>
+        <lee-tree :list="list"></lee-tree>
     </div>
 </template>
 <script>
 export default {
     data () {
         return {
+            list: [
+                {
+                    id: 1,
+                    label: '我是一级1',
+                    children: [
+                        {
+                            id: 1,
+                            label: '我是二级1-1'
+                        }
+                    ]
+                },
+                {
+                    id: 2,
+                    label: '我是一级2',
+                    children: [
+                        {
+                            id: 1,
+                            label: '我是二级2-1',
+                            children: [
+                                {
+                                    id: 1,
+                                    label: '我是二级2-1-1'
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
         }
     },
     methods: {
--- packages/leeTree/src/tree.vue
+++ packages/leeTree/src/tree.vue
@@ -1,14 +1,60 @@
 <template>
-    <div class="lee-tree">树形</div>
+    <ul class="lee-tree">
+        <li v-for="item in list" :key="item.id" @click="onTreeSelect(item)">
+            <div class="lee-tree-label">
+                <input type="checkbox" :disabled="disabled">
+                <div class="lee-tree-text">
+                    {{item.label}}
+                </div>
+            </div>
+            <lee-tree :list="item.children" v-if="item.children && item.children.length"></lee-tree>
+        </li>
+    </ul>
 </template>
 <script>
 export default {
     name: 'LeeTree',
+    props: {
+        list: {
+            type: Array,
+            default: () => {
+                return [
+                    {
+                        id: 1,
+                        label: '我是一个tree'
+                    }
+                ]
+            }
+        },
+        disabled: {
+            type: Boolean,
+            default: () => {
+                return true
+            }
+        }
+    },
     data () {
         return {}
+    },
+    methods: {
+        onTreeSelect (item) {
+            item.show = !item.show
+            this.$emit('onTreeSelect', item)
+        }
     }
 }
 </script>
 <style lang="less" scoped>
-
+.lee-tree {
+    width: 100%;
+    height: auto;
+    margin: 0;
+    li {
+        list-style: none;
+        .lee-tree-label {
+            display: flex;
+            height: 36px;
+        }
+    }
+}
 </style>
