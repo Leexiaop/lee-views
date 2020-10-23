961113bbcee7c6db52d9a175937dd29aec0f8d02
lee <xiaopeng_li@qingsongchou.com>
2020-10-23 20:25:46 +0800

    完成steps组件开发

--- examples/docs/Views.vue
+++ examples/docs/Views.vue
@@ -5,13 +5,20 @@
         <!-- <lee-divider position="left" :leftDashed="true" :rightDashed="false" leftHeight="15px" leftLineColor="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1602849726859&di=e4ba67718d65acc56a197b1af073271f&imgtype=0&src=http%3A%2F%2Fa0.att.hudong.com%2F56%2F12%2F01300000164151121576126282411.jpg"><span style="color: red;">我是分割线</span></lee-divider> -->
         <!-- <lee-input>姓名</lee-input> -->
         <!-- <lee-date-picker></lee-date-picker> -->
-        <lee-tree :list="list"></lee-tree>
+        <!-- <lee-tree :list="list"></lee-tree> -->
+        <lee-button type="danger" @click="next">下一步</lee-button>
+        <lee-steps :active="active">
+            <!-- <div slot="icon">
+                <svg t="1603447900121" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3852" width="32" height="32"><path d="M250.496 320h523.008l49.152-49.152 90.496 90.496L832 442.496V512h128v128h-128v6.464c0 38.016-6.592 74.432-18.752 108.224l99.84 99.968-90.432 90.496-78.464-78.464A319.488 319.488 0 0 1 576 960v-188.16a64 64 0 1 0-128 0V960a319.488 319.488 0 0 1-168.192-93.44l-78.464 78.528-90.496-90.496 99.904-99.968A319.424 319.424 0 0 1 192 646.464V640H64V512h128V442.496L110.848 361.344l90.496-90.496 49.152 49.152zM320 256a192 192 0 1 1 384 0H320z" fill="#262626" p-id="3853"></path></svg>
+            </div> -->
+        </lee-steps>
     </div>
 </template>
 <script>
 export default {
     data () {
         return {
+            active: 0,
             list: [
                 {
                     id: 1,
@@ -45,6 +52,9 @@ export default {
     methods: {
         handleButtonClick (e) {
             console.log(e)
+        },
+        next () {
+            this.active++
         }
     }
 }
--- packages/leeSteps/src/steps.vue
+++ packages/leeSteps/src/steps.vue
@@ -1,14 +1,137 @@
 <template>
-    <div class="lee-steps">我是步骤条</div>
+    <ul class="lee-steps" :class="{[`lee-steps-${vertical}`]: true}">
+       <li v-for="(item, index) in list" :key="index" class="lee-steps-content" :style="{color: active >= index ? activeColor : 'lightgry'}">
+            <div class="lee-steps-content-title">
+                <div v-if="$slots.icon">
+                    <slot name="icon"></slot>
+                </div>
+                <span v-else :style="{
+                    border: active >= index ? `1px solid ${activeColor}` : '1px solid #333',
+                    color: active >= index ? activeColor : '#333'
+                }">{{index + 1}}</span>
+                <i v-if="index < list.length - 1" :style="{
+                    border: active >= index ? `1px solid ${ activeColor }` : '1px solid #333'
+                }">&nbsp;</i>
+            </div>
+            <div class="lee-steps-content-content">
+                <div>{{item.title}}</div>
+                <div>{{item.description}}</div>
+            </div>
+        </li>
+    </ul>
 </template>
 <script>
 export default {
     name: 'LeeSteps',
+    props: {
+        list: {
+            type: Array,
+            default: () => {
+                return [
+                    {
+                        title: '第一步',
+                        description: '第一步要做好工作dhjahja;lhf;sahhda我就是我cada范德萨发撒风洒发生范德萨发生范德萨发生地方撒风'
+                    },
+                    {
+                        title: '第二部'
+                    },
+                    {
+                        title: '第三部'
+                    }
+                ]
+            }
+        },
+        vertical: {
+            type: String,
+            default: () => {
+                return 'row' // row, colum
+            }
+        },
+        active: {
+            type: Number,
+            default: () => {
+                return -1
+            }
+        },
+        'active-color': {
+            type: String,
+            default: () => {
+                return 'green'
+            }
+        }
+    },
     data () {
         return {}
     }
 }
 </script>
 <style lang="less" scoped>
-
+.lee-steps {
+    width: 100%;
+    padding: 0;
+    margin: 0;
+}
+.lee-steps-row {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: baseline;
+    .lee-steps-content-row
+}
+.lee-steps-colum {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: baseline;
+    .lee-steps-content-colum
+}
+.lee-steps-content-colum () {
+    li {
+        height: 100%;
+        list-style: none;
+        display: flex;
+        .lee-steps-content-title {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            span {
+                .span
+            }
+            i {
+                display: block;
+                height: calc(100% - 48px);
+                padding: 16px 0;
+            }
+        }
+    }
+}
+.lee-steps-content-row () {
+    li {
+        width: 100%;
+        list-style: none;
+    }
+    .lee-steps-content-title {
+        width: 100%;
+        display: flex;
+        align-items: center;
+        span {
+            .span
+        }
+        i {
+            display: block;
+            width: calc(100% - 48px);
+            padding: 0 16px;
+        }
+    }
+}
+.span () {
+    display: block;
+    width: 32px;
+    height: 32px;
+    line-height: 32px;
+    text-align: center;
+    border-radius: 50%;
+    border: 1px solid #333;
+}
 </style>
